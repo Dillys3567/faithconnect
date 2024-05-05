c6c0b80e39034d1caf27aaed6145290d1669e988
Dillys Annan <dillysannan@gmail.com>
2024-05-05 21:13:31 +0000

    get and display number of members

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,41 @@
+import { useState } from "react";
 import AreaChartComponent from "../../components/AreaChart/AreaChart";
 import BarChart from "../../components/BarChart/BarChart";
 import LineChart from "../../components/LineChart/LineChart";
 import SummaryCard from "../../components/SummaryCard";
 import styles from "./style.module.scss";
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = import.meta.env.VITE_API_URL;
+const supabaseKey = import.meta.env.VITE_API_KEY;
+const supabase = createClient(supabaseUrl ?? "", supabaseKey ?? "");
+
 const Dashboard = () => {
+  const [membersNum, setMembersNum] = useState(0);
+  const [malesNum, setMalesNum] = useState(0);
+  const [femalesNum, setFemalesNum] = useState(0);
+  const getNumbersOfMembers = async () => {
+    try {
+      const data = await supabase
+        .from("member")
+        .select("*", { count: "exact" });
+      console.log(data["count"]);
+      return data["count"];
+    } catch (e: any) {
+      console.log(e.message);
+    }
+  };
+
+  const fetchDataAndSetMembersNum = async () => {
+    try {
+      const membersNum = await getNumbersOfMembers();
+      setMembersNum(membersNum ?? 0);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  fetchDataAndSetMembersNum();
   return (
     <div className={styles.container}>
       <img
@@ -12,7 +44,11 @@ const Dashboard = () => {
         className={styles.big_logo}
       ></img>
       <div className={styles.summary_cards}>
-        <SummaryCard text="Total Members" number="10" type="people" />
+        <SummaryCard
+          text="Total Members"
+          number={membersNum.toString()}
+          type="people"
+        />
         <SummaryCard text="Total Male" number="10" type="male" />
         <SummaryCard text="Total Female" number="10" type="female" />
       </div>
